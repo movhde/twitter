72b2b7ff52953d45cf23ea37239de92240a92472
movhde <movahedesalehivaziri@gmail.com>
2023-11-18 10:39:38 +0330

    Make searchBox a separate component

---
+++ src/components/SideBar/SideBar/SearchBox.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+import { FiSearch } from "react-icons/fi";
+import classes from "./SearchBox.module.css";
+
+const SearchBox = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  let cssClass = classes["searchBox-container__inner"];
+
+  return (
+    <div className={classes["searchBox-container"]}>
+      <div
+        className={`${cssClass} ${isClicked ? classes.border : ""}`}
+        onClick={() => setIsClicked(true)}
+        onMouseLeave={() => setIsClicked(false)}
+      >
+        <FiSearch
+          className={`${classes.searchIcon} ${
+            isClicked ? classes["icon-color"] : ""
+          }`}
+        />
+        <input className={classes.searchBox} />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBox;
